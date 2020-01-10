068e35514f441b1e72859e483ea9c71112adc58f
Peter Johansson <peter.y.johansson@gmail.com>
2020-01-10 21:56:30 +0100

    Lagt till valuta beräkning

---
+++ MyMoney/MyMoney.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="MyMoney" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Money.aspx.cs" Inherits="Money._Default" Async="true" %>
+
+ 
+    
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>My Money</h1>
+        <p class="lead">Här kommer jag att lära mig coola saker.</p>
+        <p><button type="submit" class="btn btn-primary btn-lg" id="UpdateStockPrice" runat="server" onserverclick="UpdateStockPrice_onclickAsync">Uppdatera kurser &raquo;</button></p>
+    
+    </div>
+
+    
+    <asp:Repeater id="Repeater" runat="server">
+        <HeaderTemplate>
+                <table class="table table-striped" >
+                <tr>
+                <th>Aktie</th>
+                <th>Antal</th>
+                <th>GAVKurs</th>
+                <th>Kurs</th>
+                <th>Summa</th>
+                </tr>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <tr>
+            <td><%# DataBinder.Eval(Container.DataItem,"Investment") %> </td>
+            <td><%# DataBinder.Eval(Container.DataItem,"Antal") %></td>
+            <td><%# DataBinder.Eval(Container.DataItem,"GAVKurs") %></td>
+            <td><%# DataBinder.Eval(Container.DataItem,"Kurs") %></td>
+            <td><%# DataBinder.Eval(Container.DataItem,"Summa") %></td>
+            </tr>
+        </ItemTemplate>
+        <FooterTemplate>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+
+
+
+
+<%--        <div class="form-group">
+         <label for="NAnting" class="col-sm-3 control-label" >Aktier:</label>
+         <div class="col-sm-8">
+                 <asp:DropDownList class="form-control" ID="drpDownNames" value="" runat="server" readonly="true">
+                 </asp:DropDownList>
+          </div>
+   </div>--%>
+
+               
+
+</asp:Content>
+
+
---
+++ MyMoney/MyMoney.aspx.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace Money
+{
+    public partial class _Default : Page
+    {
+        string conString = WebConfigurationManager.AppSettings["MySqlConnectionString"];
+        const string quote = "\"";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Logger("INFO", "Nu kör vi igång!");
+            getIPSTable();
+            
+        }
+
+
+        private void getIPSTable()
+        {
+
+            string mysqlcmnd = "SELECT * FROM money.ips order by Investment;";
+            DataTable dt = new DataTable();
+            float rate = 1;
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(conString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand myCommand = new MySqlCommand(mysqlcmnd, connection))
+                    {
+
+                        using (MySqlDataAdapter mysqlDa = new MySqlDataAdapter(myCommand))
+                            mysqlDa.Fill(dt);
+
+                        Repeater.DataSource = dt;
+                        dt.Columns.Add("Summa", typeof(float));
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            var Antal = row[2];
+                            var Kurs = row[3];
+                            string Valuta = row[8].ToString();
+                            if (string.Compare(Valuta, "SEK") != 0)
+                            {
+                                rate = ConvertExchangeRates(Valuta);
+                            }
+
+                            float Summa = (float)Antal * (float)Kurs * rate;
+                            row[10] = Math.Round(Summa, 0);
+                        }
+
+                        Repeater.DataBind();
+
+                        //drpDownNames.DataSource = dt;
+                        //drpDownNames.DataTextField = dt.Columns["Investment"].ToString();
+                        //drpDownNames.DataValueField = dt.Columns["Antal"].ToString();
+                        //drpDownNames.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger("ERROR", ex.Message);
+            }
+        }
+
+        public class AlphaVantageData
+        {
+            public DateTime Timestamp { get; set; }
+            public decimal Open { get; set; }
+
+            public decimal High { get; set; }
+            public decimal Low { get; set; }
+
+            public decimal Close { get; set; }
+            public decimal Volume { get; set; }
+        }
+
+        public class AlphaVantageBatchData
+        {
+
+            public string symbol { get; set; }
+            public decimal price { get; set; }
+            public string volume { get; set; }
+            public DateTime Timestamp { get; set; }
+        }
+
+        public void UpdateStock(string symbol, decimal Kurs)
+        {
+
+            string mysqlcmnd = "UPDATE money.ips SET Kurs =  " + Kurs + " WHERE Symbol = " + quote + symbol + quote + ";";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(conString))
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand(mysqlcmnd, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger("ERROR", ex.Message);
+            }
+        }
+
+        //public void UpdateStockPrice_onclickAV(object sender, EventArgs e)
+        //{
+        //    //  Welcome to Alpha Vantage! Here is your API key: APA3UI90FWXJA9IM
+        //    //  string ApiURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=APA3UI90FWXJA9IM";
+        //    //  https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&apikey=xxx&symbols=MSFT,AAPL,FB
+        //    // NOTERA: För att använda detta så måste stockholms börsens aktier markeras med STO och inte ST
+
+        //    string mysqlcmnd = "SELECT * FROM money.ips;";
+        //    DataTable dt = new DataTable();
+
+        //    //var symbol = "TSLA";
+        //    var apiKey = "APA3UI90FWXJA9IM";
+        //    //var MultiPrices = $"https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&apikey={apiKey}&symbols=MSFT,AAPL,FB&datatype=csv".GetStringFromUrl().FromCsv<List<AlphaVantageBatchData>>();
+        //    //var Symb = MultiPrices.First().symbol;
+
+
+        //    try
+        //    {
+        //        using (MySqlConnection connection = new MySqlConnection(conString))
+        //        {
+        //            connection.Open();
+
+        //            using (MySqlCommand myCommand = new MySqlCommand(mysqlcmnd, connection))
+        //            {
+
+        //                using (MySqlDataAdapter mysqlDa = new MySqlDataAdapter(myCommand))
+        //                mysqlDa.Fill(dt);
+
+        //                foreach (DataRow row in dt.Rows)
+        //                {
+        //                    string symbol = row[9].ToString();
+
+        //                    try
+        //                    {
+        //                        var Prices = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={apiKey}&datatype=csv".GetStringFromUrl().FromCsv<List<AlphaVantageData>>();
+        //                        var CurrentOpenPrice = Prices.First().Open;
+        //                        UpdateStock(symbol, CurrentOpenPrice);
+
+        //                        System.Threading.Thread.Sleep(20000);
+
+        //                        Logger("DEBUG", symbol + " " + CurrentOpenPrice);
+        //                    }
+        //                    catch (Exception ex)
+        //                    {
+        //                        Logger("ERROR", symbol + " " + ex.Message);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Logger("ERROR", ex.Message);
+        //    }
+        //}
+
+
+        public class FinnhubData
+        {
+            public decimal C { get; set; }
+            public decimal H { get; set; }
+            public decimal L { get; set; }
+            public decimal O { get; set; }
+            public decimal PC { get; set; }
+        }
+
+        public class ExchangerateData
+        {
+            public string b { get; set; }
+            DateTime datetime { get; set; }
+            public string[] rates { get; set; }
+        }
+
+        public async void UpdateStockPrice_onclickAsync(object sender, EventArgs e)
+        {
+
+            // key: bo5suuvrh5rbvm1sl1t0   https://finnhub.io/dashboard
+            // https://finnhub.io/api/v1/quote?symbol=AAPL&token=bo5suuvrh5rbvm1sl1t0
+
+            string mysqlcmnd = "SELECT * FROM money.ips;";
+            string apiKey = "";
+
+            DataTable dt = new DataTable();
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(conString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand myCommand = new MySqlCommand(mysqlcmnd, connection))
+                    {
+                        using (MySqlDataAdapter mysqlDa = new MySqlDataAdapter(myCommand))
+                            mysqlDa.Fill(dt);
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            string symbol = row[9].ToString();
+
+                            //Logger("DEBUG", "Symbol: " + symbol);
+
+                            try
+                            {
+
+                                string url = $"https://finnhub.io/api/v1/quote?symbol={symbol}&token={apiKey}";
+
+                                string responseBody = await client.GetStringAsync(url);
+                                FinnhubData StockData = JsonConvert.DeserializeObject<FinnhubData>(responseBody);
+                                decimal CurrentOpenPrice = StockData.C;
+
+                                UpdateStock(symbol, CurrentOpenPrice);
+                                Logger("DEBUG", symbol + " " + CurrentOpenPrice);
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger("ERROR", symbol + " " + ex.Message);
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger("ERROR", ex.Message);
+            }
+
+            // Reload the table
+            getIPSTable();
+        }
+
+        public float ConvertExchangeRates(string Valuta)
+        {
+            //https://api.exchangeratesapi.io/latest?base=USD
+
+            string url = $"https://api.exchangeratesapi.io/latest?base={Valuta}";
+            var client = new System.Net.WebClient();
+            
+            try
+            {
+                string responseBody = client.DownloadString(url);
+                int position = responseBody.IndexOf("SEK");
+                string substring = responseBody.Substring(position + 5, 20);
+                int endposition = substring.IndexOf(",");
+                string rate = substring.Substring(0, endposition - 1);
+                return float.Parse(rate);
+
+            }
+            catch (Exception ex)
+            {
+                Logger("ERROR", "Problem med att hämta Exchange rates " + ex.Message);
+                return 0;
+            }
+
+        }
+
+        public void Logger(string type, string message)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(conString))
+                {
+                    connection.Open();
+
+                    string CommandText = "INSERT INTO log set type = @type, message = @message";
+                    MySqlCommand command = new MySqlCommand(CommandText, connection);
+
+                    command.Parameters.AddWithValue("@type", type);
+                    command.Parameters.AddWithValue("@message", message);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
